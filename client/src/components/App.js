170ab1a37675ea55d42302daa894493b5b29af61
cheng xiang <chengxiang.seet@tcs.com>
2021-05-06 23:03:31 +0800

    minor changes to App.js to ensure react app working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
 
 import './App.css';
 import PrivateRoute from './PrivateRoute';
@@ -10,18 +11,19 @@ function App() {
       <Switch>
         <Route path="/" />
       </Switch>
-      {/* Main body Component - Make Routes here */}
+      {/* Main body Component - Make Routes here */}    
       <Switch>
-        <Route exact path="/loginPage" />
+        <Route exact path="/loginPage" />       
         <Route exact path="/">
-          <Redirect to="/LoginPage" />
+          {/* <Redirect to="/LoginPage" /> */}
+          <div>delete me after adding your own routes, this is just to test the page renders</div>
         </Route>
         {/* Add Private routes for all pages only accessible after login */}
         <PrivateRoute exact path="/" />
         <PrivateRoute exact path="/" />
       </Switch>
       {/* Footer Component */}
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
