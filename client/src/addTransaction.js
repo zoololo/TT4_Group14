b1c063fb30579c4a4e4c1772a51275914fe9c9d8
Pack-Yak1 <wang_yangwu@hotmail.com>
2021-05-07 13:41:03 +0800

    Fixed type issues
    - Can now correctly send APIs

--- client/src/addTransaction.js
+++ client/src/addTransaction.js
@@ -5,50 +5,62 @@ class addTransaction extends React.Component{
         super(props);
         this.state = {
             // To be posted when transaction added
-            "custID": '',
-            "accountKey": '',
+            custID: 14,
+            accountKey: 'g1la0msi-ennf-t692-winw-b1h2utx82p2',
             // To be filled in by user
-            "payeeID": '',
-            "amount": '',
-            "eGift": false,
-            "message": ''
+            payeeID: '',
+            amount: '',
+            eGift: false,
+            message: ''
         };
         
         this.create = this.create.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    // Retrieve the user's `custID` and `accountKey`, given userName, Pass
-    componentDidMount(){
-        fetch("https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/login",{
-            "method": "POST",
-            "headers": {
-                'x-api-key': "ykOwd1IKUR3bX1I7O3yWx6QomMSqTOrG2cKUdzhg"
-            },
-            "body": {
-                "userName": "Group14",
-                "accountKey": "BgQ%o_rF0$Fkv2U"
-            }
-        })
-        .then(response => response.json())
-        .then(response => {
-            this.setState({
-                custID: response['custID'],
-                accountKey: response['accountKey']
-            })
-        })
-        .catch(err => {console.log(err)});
-    }
+    // // Retrieve the user's `custID` and `accountKey`, given userName, Pass
+    // componentDidMount(){
+    //     fetch("https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/login",{
+    //         "method": "POST",
+    //         "headers": {
+    //             'x-api-key': "ykOwd1IKUR3bX1I7O3yWx6QomMSqTOrG2cKUdzhg"
+    //         },
+    //         "body": {
+    //             "userName": "Group14",
+    //             "accountKey": "BgQ%o_rF0$Fkv2U"
+    //         }
+    //     })
+    //     .then(response => response.json())
+    //     .then(response => {
+    //         this.setState({
+    //             custID: response['custID'],
+    //             accountKey: response['accountKey']
+    //         })
+    //     })
+    //     .catch(err => {console.log(err)});
+    // }
 
     create(e) {
         e.preventDefault();
 
+        if (typeof this.state.payeeID != "number"){
+            console.log("Please enter only numbers as Payee ID.")
+        } else if (typeof this.state.amount != "number"){
+            console.log("Please enter only numbers as Amount.")
+        }
         fetch("https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add",{
             "method": "POST",
             "headers": {
                 'x-api-key': "ykOwd1IKUR3bX1I7O3yWx6QomMSqTOrG2cKUdzhg"
             },
-            "body": this.state
+            "body": JSON.stringify({
+                custID: this.state.custID,
+                accountKey: this.state.accountKey,
+                payeeID: this.state.payeeID,
+                amount: this.state.amount,
+                eGift: this.state.eGift,
+                message: this.state.message
+            })
         })
         .then(response => response.json)
         // TODO: Display success message, maybe redirect to homepage
@@ -79,10 +91,10 @@ class addTransaction extends React.Component{
                                 <input 
                                     name="payeeID"
                                     id="payeeID"
-                                    type="text"
+                                    type="number"
                                     className="form-control"
                                     value={this.state.name}
-                                    onChange={(e) => this.handleChange({payeeID: e.target.value})}
+                                    onChange={(e) => this.handleChange({payeeID: parseInt(e.target.value)})}
                                     required
                                 />
                             </label>
@@ -91,20 +103,20 @@ class addTransaction extends React.Component{
                                 <input 
                                     name="amount"
                                     id="amount"
-                                    type="text"
+                                    type="number"
                                     className="form-control"
                                     value={this.state.name}
-                                    onChange={(e) => this.handleChange({amount: e.target.value})}
+                                    onChange={(e) => this.handleChange({amount: parseInt(e.target.value)})}
                                     required
                                 />
                             </label>
                             {/* TODO: make this a yes/no button */}
                             <label htmlFor="gift">
-                                Is this a gift?:
+                                Is this an eGift?: 
                                 <input
                                     name="This is an eGift"
                                     type="checkbox"
-                                    onChange={(e) => this.handleChange({eGift: e.target.value})}
+                                    onChange={(e) => this.handleChange({eGift: e.target.value == "on" ? true : false})}
                                 />
                             </label>
                             <label htmlFor="msg">
