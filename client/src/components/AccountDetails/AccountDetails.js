3670c66a630f59ccb0313f6b067e693517006932
cheng xiang <chengxiang.seet@tcs.com>
2021-05-07 15:31:58 +0800

    misc

--- client/src/components/AccountDetails/AccountDetails.js
+++ client/src/components/AccountDetails/AccountDetails.js
@@ -1,38 +1,46 @@
 import React, { useState } from 'react';
 
-import { withRouter } from 'react-router-dom';
-
 import axios from 'axios';
 
  
 
 const AccountDetails = () => {
 
-    const [bankBalance, setBankBalance] = useState(null);
+    const [availableBal, setAvailableBal] = useState(null);
+
+    const [accountName, setAccountName] = useState(null);
+
+    const [accountNumber, setAccountNumber] = useState(null);
 
  
 
-    if (localStorage.getItem("accountKey")) {
+    if (localStorage.getItem('accountKey')){
 
-        const key = sessionStorage.getItem("accountKey");
- 
+        const key = localStorage.getItem('accountKey')
 
-        const checkBalance = async (key) => {
+        
 
-            const payload = {
+        const checkDetails = async (key) => {
+
+            const payload = { 
 
                 custID : localStorage.getItem('custID'),
 
-                accountKey : localStorage.getItem('accountKey')}
+                accountKey : localStorage.getItem('accountKey')
+
+            }
 
             const response = await axios.post("https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/accounts", payload, { headers : {'x-api-key' : "ykOwd1IKUR3bX1I7O3yWx6QomMSqTOrG2cKUdzhg"}})
-            console.log(response);
-            console.log(payload);
-            setBankBalance(response.data.bankBalance);
+
+            setAccountName(response.data.accountName)
+
+            setAccountNumber(response.data.accountNumber)
+
+            setAvailableBal(response.data.availableBal)
 
         }
 
-        checkBalance(key)
+        checkDetails(key)
 
     }
 
@@ -40,28 +48,22 @@ const AccountDetails = () => {
 
     return(
 
-    <div className="container" style={{border:"solid black 5px"}}>
-
-        <h1>BALANCE</h1>
-
-        <br/>
-
-        Bank Balance : {bankBalance ? 
-
         <div>
 
-            <h2>{bankBalance}</h2>
+            <h1>Account Details:</h1>
+
+            <p>Name: {accountName}</p>
+
+            <p>Number: {accountNumber}</p>
+
+            <p>Balance: {availableBal}</p>
 
         </div>
 
-         : null }
-
-    </div>
-
-    );
+    )
 
 }
 
  
 
-export default withRouter(AccountDetails);
+export default AccountDetails;
